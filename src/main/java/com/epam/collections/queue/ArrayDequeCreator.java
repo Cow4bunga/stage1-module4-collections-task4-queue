29d09582df7ea480acd4df8f191911118b42d304
Ivan Levitsky <yavanyaprivet@gmail.com>
2022-09-13 13:40:10 +0300

    done

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,20 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> gameDeck=new ArrayDeque<>();
+
+        gameDeck.add(firstQueue.poll());
+        gameDeck.add(firstQueue.poll());
+        gameDeck.add(secondQueue.poll());
+        gameDeck.add(secondQueue.poll());
+        while(!firstQueue.isEmpty() && !secondQueue.isEmpty()){
+            firstQueue.offer(gameDeck.removeLast());
+            gameDeck.add(firstQueue.poll());
+            gameDeck.add(firstQueue.poll());
+            secondQueue.offer(gameDeck.removeLast());
+            gameDeck.add(secondQueue.poll());
+            gameDeck.add(secondQueue.poll());
+        }
+        return gameDeck;
     }
 }
